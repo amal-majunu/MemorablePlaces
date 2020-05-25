fd1fb06d428a0f47cdeed3c63422e587a93e3eaa
amal-majunu <amalmajunu2001@gmail.com>
2020-05-25 12:41:55 +0530

    Added App

---
+++ app/src/main/java/com/example/android/memorableplaces/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.android.memorableplaces;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    static ArrayList<String> places = new ArrayList<String>();
+    static ArrayList<LatLng> locations = new ArrayList<>();
+    static ArrayAdapter arrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView listView = (ListView)findViewById(R.id.list);
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.android.memorableplaces", Context.MODE_PRIVATE);
+
+        ArrayList<String> latitudes = new ArrayList<>();
+        ArrayList<String> longitudes = new ArrayList<>();
+
+        places.clear();
+        latitudes.clear();
+        longitudes.clear();
+        locations.clear();
+
+        try {
+            places = (ArrayList<String>)ObjectSerializer.deserialize(sharedPreferences.getString("places", ObjectSerializer.serialize(new ArrayList<String>())));
+            latitudes = (ArrayList<String>)ObjectSerializer.deserialize(sharedPreferences.getString("latitudes", ObjectSerializer.serialize(new ArrayList<String>())));
+            longitudes = (ArrayList<String>)ObjectSerializer.deserialize(sharedPreferences.getString("longitudes", ObjectSerializer.serialize(new ArrayList<String>())));
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        if(places.size()>0 && latitudes.size()>0 && longitudes.size()>0){
+            if(places.size()==latitudes.size() && latitudes.size()==longitudes.size()){
+                for(int i=0;i<places.size();i++) {
+                    locations.add(new LatLng(Double.parseDouble(latitudes.get(i)), Double.parseDouble(longitudes.get(i))));
+                }
+            }
+        }
+        else {
+            places.add("Add new place...");
+            locations.add(new LatLng(0, 0));
+        }
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,places);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+                Intent intent = new Intent(getApplicationContext(),MapsActivity.class);
+                intent.putExtra("number", i);
+                startActivity(intent);
+            }
+        });
+    }
+}
